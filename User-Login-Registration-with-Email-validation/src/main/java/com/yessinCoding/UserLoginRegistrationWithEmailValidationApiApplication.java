bc3a41c7fbf1c3e053b46933012e8166f7f6e1a9
yassine <rezguiyassine224@gmail.com>
2025-03-16 15:32:07 +0100

    first commit

---
+++ User-Login-Registration-with-Email-validation/src/main/java/com/yessinCoding/UserLoginRegistrationWithEmailValidationApiApplication.java
@@ -0,0 +1,13 @@
+package com.yessinCoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegistrationWithEmailValidationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginRegistrationWithEmailValidationApiApplication.class, args);
+	}
+
+}
---
+++ User-Login-Registration-with-Email-validation/src/test/java/com/yessinCoding/UserLoginRegistrationWithEmailValidationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yessinCoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginRegistrationWithEmailValidationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
